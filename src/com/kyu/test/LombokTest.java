548ee99385ab19d2564275c1136e67832d02b984
이남규 <@kyu-PC>
2012-10-24 23:54:57 +0900

    lombok 테스트

---
+++ src/com/kyu/test/LombokTest.java
@@ -0,0 +1,18 @@
+package com.kyu.test;
+
+import lombok.Data;
+
+/**
+ * @FileName : LombokTest.java
+ * @Project : sample_project
+ * @Date : 2012. 10. 22.
+ * @작성자 : 이남규
+ * @프로그램설명 :
+ */
+@Data
+public class LombokTest {
+
+	private String name;
+	private String id;
+
+}
